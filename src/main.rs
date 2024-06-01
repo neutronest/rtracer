a91fae34d7aeb0142ed23f6b3e4dd44c255227ed
neutronest <neutronest@gmail.com>
2024-06-01 18:36:47 +1000

    update image ppm output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello World");
+    // Image
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+
+    // Render
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f32 / (image_width -1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0 as f32;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
